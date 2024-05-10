a6da831b33d316c2e02d444eef9839b8d5bcfe9e
DavidSheridan <dpmsheridan@hotmail.com>
2024-05-10 14:30:27 +1200

    Updated the setup page and welcome page to make it more clear to run the lookup populating report

--- criblvision/appserver/static/javascript/views/app.js
+++ criblvision/appserver/static/javascript/views/app.js
@@ -29,9 +29,11 @@ define(['react', 'splunkjs/splunk'], function(react, splunk_js_sdk){
     }
 
     render(){
-      return e('div', null, [
+      return e('div', { style: { textAlign: 'center' } }, [
+        e('h1', null, 'CriblVision Setup'),
+        e('hr', { style: { borderTop: '3px solid #bbb' } }),
         e('div', null, [
-          e('h1', null, 'Logs and Metrics'),
+          e('h2', { style: { fontWeight: 'bold' } }, 'Logs and Metrics'),
           e('p', null, [
             'Cribl internal logs and metrics must be enabled and forwarded to Splunk in order for all of the panels to populate with events. Refer to the documentation ',
             e('a', { href: 'https://docs.cribl.io/stream/sources-cribl-internal/#configuring-cribl-internal-logsmetrics-as-a-datasource', target: '_blank' }, 'here'),
@@ -39,37 +41,29 @@ define(['react', 'splunkjs/splunk'], function(react, splunk_js_sdk){
           ])
         ]),
         e('div', null, [
-          e('h1', null, 'Macros'),
+          e('h2', { style: { fontWeight: 'bold' } }, 'Macros'),
           e('p', null, 'This app ships with four macros which must be configured in accordance with your Splunk index naming schema:'),
-          e('form', null, [
-            e('p', null, [
-              e('label', null, [
-                'Cribl Internal Log Splunk Index: ',
-                e('input', { type: 'text', name: 'set_cribl_internal_log_index', value: this.state.set_cribl_internal_log_index, onChange: this.handleChange })
-              ])
+          e('div', { style: { display: 'block' } }, e('form', { style: { display: 'table', marginLeft: 'auto', marginRight: 'auto' } }, [
+            e('p', { style: { display: 'table-row' } }, [
+              e('label', { style: { display: 'table-cell', textAlign: 'left', marginBottom: '1em', paddingRight: '1em' } }, 'Cribl Internal Log Splunk Index:'),
+              e('input', { style: { display: 'table-cell', marginBottom: '1em' }, type: 'text', name: 'set_cribl_internal_log_index', value: this.state.set_cribl_internal_log_index, onChange: this.handleChange })
             ]),
-            e('p', null, [
-              e('label', null, [
-                'Cribl Internal Log Sourcetype(s): ',
-                e('input', { type: 'text', name: 'set_cribl_log_sourcetype', value: this.state.set_cribl_log_sourcetype, onChange: this.handleChange })
-              ])
+            e('p', { style: { display: 'table-row' } }, [
+              e('label', { style: { display: 'table-cell', textAlign: 'left', marginBottom: '1em', paddingRight: '1em' } }, 'Cribl Internal Log Sourcetype(s):'),
+              e('input', { style: { display: 'table-cell', marginBottom: '1em' }, type: 'text', name: 'set_cribl_log_sourcetype', value: this.state.set_cribl_log_sourcetype, onChange: this.handleChange })
             ]),
-            e('p', null, [
-              e('label', null, [
-                'Cribl Metrics Splunk Index: ',
-                e('input', { type: 'text', name: 'set_cribl_metrics_index', value: this.state.set_cribl_metrics_index, onChange: this.handleChange })
-              ])
+            e('p', { style: { display: 'table-row' } }, [
+              e('label', { style: { display: 'table-cell', textAlign: 'left', marginBottom: '1em', paddingRight: '1em' } }, 'Cribl Metrics Splunk Index:'),
+              e('input', { type: 'text', name: 'set_cribl_metrics_index', value: this.state.set_cribl_metrics_index, onChange: this.handleChange })
             ]),
-            e('p', null, [
-              e('label', null, [
-                'Cribl Metrics Prefix: ',
-                e('input', { type: 'text', name: 'set_cribl_metrics_prefix', value: this.state.set_cribl_metrics_prefix, onChange: this.handleChange })
-              ])
+            e('p', { style: { display: 'table-row' } }, [
+              e('label', { style: { display: 'table-cell', textAlign: 'left', marginBottom: '1em', paddingRight: '1em' } }, 'Cribl Metrics Prefix:'),
+              e('input', { style: { display: 'table-cell', marginBottom: '1em' }, type: 'text', name: 'set_cribl_metrics_prefix', value: this.state.set_cribl_metrics_prefix, onChange: this.handleChange })
             ])
-          ])
+          ]))
         ]),
         e('div', null, [
-          e('h1', null, 'Leader Logs'),
+          e('h2', { style: { fontWeight: 'bold' } }, 'Leader Logs'),
           e('p', null, [
             'Some of the views in this app will require Leader logs to be forwarded onto Splunk. In distributed Cribl Stream environments, Leader logs are currently ',
             e('b', null, 'NOT'),
@@ -82,7 +76,12 @@ define(['react', 'splunkjs/splunk'], function(react, splunk_js_sdk){
             ' When deploying the Edge node to your Leader node, we recommend having a separate fleet just for this node. Be sure to disable all other inputs on that Edge node except for file monitor inputs.'
           ])
         ]),
-        e('button', { onClick: this.handleSubmit }, 'Save Configuration')
+        e('div', null, [
+          e('h2', { style: { fontWeight: 'bold' } }, 'Cribl Stream Assets Lookup'),
+          e('p', null, 'The CriblVision app utilizes a lookup to add context to its searches. This needs to be created to have the dashboards, searches, and reports work as expected. To create this lookup, either follow the instructions on the Welcome page after saving this configuration, or click the "Populate Cribl Stream Asset Lookup" item in the navigation bar.')
+        ]),
+        e('hr', { style: { borderTop: '3px solid #bbb' } }),
+        e('button', { className: 'btn btn-primary', onClick: this.handleSubmit }, 'Save Configuration')
       ]);
     }
   }
